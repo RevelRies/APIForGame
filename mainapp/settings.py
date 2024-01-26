afafcbe0f3827fd6e936a868f1e22626cdb6591d
RevelRies <ayzikov070@mail.ru>
2024-01-26 10:15:16 +0300

    init: add game app, add apps in settings

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game.apps.GameConfig',
+    'apiv1.apps.Apiv1Config',
 ]
 
 MIDDLEWARE = [
