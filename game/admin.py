7f2f5341b6e6daf5e7a1e8dd677f00a5f124c5e9
RevelRies <ayzikov070@mail.ru>
2024-01-26 14:25:56 +0300

    refactor: edit game models

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserSeasonScore, UserProfile, Season
+
+admin.site.register(UserProfile)
+admin.site.register(UserSeasonScore)
+admin.site.register(Season)
--- game/models.py
+++ game/models.py
@@ -8,6 +8,13 @@ class UserProfile(models.Model):
     all_time_high_score = models.IntegerField(default=0, verbose_name='максимальный результат за все время')
     coins = models.IntegerField(default=0, verbose_name='количество монет у пользователя')
 
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
+
+    def __str__(self):
+        return f"Профиль пользователя {self.user.username}"
+
 
 class Season(models.Model):
     name = models.CharField(max_length=50, verbose_name='название сезона')
@@ -16,9 +23,24 @@ class Season(models.Model):
     finish_date = models.DateTimeField(verbose_name='время окончания сезона')
     prize = models.CharField(max_length=250, blank=True, verbose_name='приз сезона')
 
+    class Meta:
+        verbose_name = 'Сезон'
+        verbose_name_plural = 'Сезоны'
+
+    def __str__(self):
+        return f"{self.number} сезон"
+
 
 class UserSeasonScore(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    season = models.ForeignKey(to=Season, on_delete=models.CASCADE)
+    # related_name позволяет назначать имя атрибуту, который используется для связи от ассоциированного объекта назад к нему.
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='user_score')
+    season = models.ForeignKey(to=Season, on_delete=models.CASCADE, related_name='season_score')
     season_high_score = models.IntegerField(default=0, verbose_name='максимальный результат пользователя в сезоне')
+
+    class Meta:
+        verbose_name = 'Результат за сезон'
+        verbose_name_plural = 'Результаты за сезоны'
+
+    def __str__(self):
+        return f"Результаты за сезон {self.user.username}"
 
