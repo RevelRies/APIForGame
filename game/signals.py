8da25e07e8f92ff25df69321a8f58aab77a40e52
RevelRies <ayzikov070@mail.ru>
2024-01-31 14:59:19 +0300

    feat: now, when creating a user, a UserSeasonScore is created for the current Season

--- game/signals.py
+++ game/signals.py
@@ -1,12 +1,33 @@
+
+from django.utils import timezone
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
 
-from .models import Season, UserSeasonScore
+from .models import User, Season, UserSeasonScore
 
 @receiver(post_save, sender=Season)
 def create_user_season_score(sender, instance, created, **kwargs):
+    '''
+    Функция создает UserSeasonScore для каждого пользователя при создании нового сезона
+    '''
     if created:
         for user in User.objects.all():
             UserSeasonScore.objects.create(user=user,
                                            season=instance)
+
+@receiver(post_save, sender=User)
+def create_new_user_season_score(sender, instance, created, **kwargs):
+    '''
+    Функция создает UserSeasonScore для текущего сезона только что зарегистрировавшегося пользователя
+    '''
+
+    current_season = Season.objects.filter(
+        start_date__lte=timezone.now(),
+        finish_date__gte=timezone.now()
+    ).first()
+
+    if created:
+        UserSeasonScore.objects.create(
+            user=instance,
+            season=current_season
+        )
