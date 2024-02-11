04440a07ea838537ff351ac8df4f11928fc1aa63
RevelRies <ayzikov070@mail.ru>
2024-02-11 13:16:23 +0300

    refactor: changed the sorting of the display of models in the admin panel

--- game/admin.py
+++ game/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 
 from .models import UserSeasonScore, User, Season
 
-admin.site.register(User)
-admin.site.register(UserSeasonScore)
-admin.site.register(Season)
+@admin.register(User)
+class UserPost(admin.ModelAdmin):
+    # пользователи будут автоматически сортироваться по username
+    ordering = ['username']
+    # определяет боковую панель с полями по которым можно включить фильтрацию
+    list_filter = ['username', 'date_joined']
+
+
+@admin.register(UserSeasonScore)
+class UserSeasonScorePost(admin.ModelAdmin):
+    # пользователи будут автоматически сортироваться по username
+    ordering = ['-season__number', 'user__username']
+
+@admin.register(Season)
+class SeasonPost(admin.ModelAdmin):
+    # пользователи будут автоматически сортироваться по username
+    ordering = ['-number']
