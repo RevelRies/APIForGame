f463925a10423546d9c53da36162de5226e42796
root <root@gameapi.com>
2024-02-11 10:03:30 +0000

    refactor: edit gunicorn comfiguration

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /var/www/gameapi_01/env/bin/activate
+source /var/www/gameapi_01/APIForGame/env/bin/activate
 exec gunicorn -c "/var/www/gameapi_01/APIForGame/mainapp/gunicorn_config.py" mainapp.wsgi
--- mainapp/gunicorn_config.py
+++ mainapp/gunicorn_config.py
@@ -1,6 +1,6 @@
 command = '/var/www/gameapi_01/APIForGame/env/bin/gunicorn'
 python_path = '/var/www/gameapi_01/APIForGame/mainapp'
-bind = '0.0.0.0:8001'
+bind = '127.0.0.1:8001'
 workers = 5
 user = 'www'
 raw_env = 'DJANGO_SETTINGS_MODULE=mainapp.settings'
