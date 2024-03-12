3cc96dcfaf9fa5f0e11be8658eeb35f7746a9b17
RevelRies <ayzikov070@mail.ru>
2024-03-12 21:49:27 +0300

    fix: edit function for create new season

---
+++ game/management/commands/add_new_season.py
@@ -0,0 +1,44 @@
+from datetime import timedelta
+
+from game.models import Season
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = ('Каждый день проверяет сколько осталось до конца текущего сезона'
+            'Если осталась 1 минута, то функция создает новый сезон и делает его активным'
+            'Проверка происходит в 23:58 по системному времени')
+
+    def handle(self, *args, **options):
+        print("Отрабатывает команда создания сезона")
+
+        # Проверяем равен ли день конца сезона сегодняшнему дню
+        prev_season = Season.objects.get(is_active=True)
+        current_day = timezone.now().day
+        # Если конец сезона сегодня, то проверяем законфигурирован ли следующий сезон
+        if prev_season.finish_date.day == current_day:
+            # Если следующий сезон не законфигурирован, то создаем новый и делаем его активным
+            if Season.objects.get(is_active=True) == Season.objects.all().last():
+                number = prev_season.number + 1
+                name = f"season_{number}"
+                # берем текущее время (23:58), прибавляем к нему час, заменяем часы и минуты на нули
+                start_date = (timezone.localtime(timezone.now()) + timedelta(days=1)).replace(hour=0, minute=0, second=0)
+                # продолжительность предыдущего сезона в днях
+                length_prev_season = (prev_season.finish_date - prev_season.start_date).days
+                # прибавляем к текущему времени продолжительность предыдущего сезона
+                finish_date = ((timezone.localtime(timezone.now()) + timedelta(days=length_prev_season))
+                               .replace(hour=23, minute=58, second=0))
+    
+                # создаем новый сезон
+                Season.objects.create(name=name, number=number, start_date=start_date, finish_date=finish_date,
+                                      is_active=True)
+            # если новый сезон уже законфигурирован, то просто делаем его активным
+            else:
+                next_season = Season.objects.get(number=prev_season.number + 1)
+                next_season.is_active = True
+                next_season.save()
+
+            prev_season.is_active = False
+            prev_season.save()
+
---
+++ scheduler.py
@@ -0,0 +1,8 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.core.management import call_command
+from pytz import timezone
+
+def start():
+    scheduler = BackgroundScheduler(timezone=timezone('Europe/Moscow'))
+    scheduler.add_job(call_command, 'cron', hour=23, minute=58, args=['add_new_season'])
+    scheduler.start()
